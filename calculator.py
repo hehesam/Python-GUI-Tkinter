05d5f663c844a55d72c937ff239c5f80c2223e37
Hesam <mohebi.hesam@gmail.com>
2022-06-29 22:26:11 +0430

    added calculator

--- calculator.py
+++ calculator.py
@@ -4,31 +4,96 @@ root = Tk()
 root.title("calculator")
 e = Entry(root, width=35, borderwidth=5)
 e.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
-e.insert(0, "Enter your name :")
+global f_num
+global math
+
+def button_click(number):
+    current = e.get()
+    e.delete(0,END)
+    e.insert(0, current + str(number))
+
+def button_clear():
+    e.delete(0, END)
 
 def button_add():
-    return
+    first_number = e.get()
+    global f_num
+    global  math
+    math = "addition"
+    f_num = int(first_number)
+    e.delete(0,END)
 
-button_1 = Button(root, text='1', padx=40, pady=20, command=button_add)
-button_2 = Button(root, text='2', padx=40, pady=20, command=button_add)
-button_3 = Button(root, text='3', padx=40, pady=20, command=button_add)
-button_4 = Button(root, text='4', padx=40, pady=20, command=button_add)
-button_5 = Button(root, text='5', padx=40, pady=20, command=button_add)
-button_6 = Button(root, text='6', padx=40, pady=20, command=button_add)
-button_7 = Button(root, text='7', padx=40, pady=20, command=button_add)
-button_8 = Button(root, text='8', padx=40, pady=20, command=button_add)
-button_9 = Button(root, text='9', padx=40, pady=20, command=button_add)
-button_0 = Button(root, text='0', padx=40, pady=20, command=button_add)
-button_add = Button(root, text='+', padx=39, pady=20, command=button_add)
-button_equal = Button(root, text='=', padx=91, pady=20, command=button_add)
-button_clear = Button(root, text='clear', padx=79, pady=20, command=button_add)
+def button_subtract():
+    first_number = e.get()
+    global f_num
+    global  math
+    math = "subtraction"
+    f_num = int(first_number)
+    e.delete(0,END)
+def button_multiply():
+    first_number = e.get()
+    global f_num
+    global  math
+    math = "multiplication"
+    f_num = int(first_number)
+    e.delete(0,END)
+
+def button_divide():
+    first_number = e.get()
+    print(first_number)
+    global f_num
+    global  math
+    math = "division"
+    f_num = int(first_number)
+    e.delete(0,END)
+
+def button_eql():
+    sec_num = e.get()
+    second_number = int(sec_num)
+    e.delete(0,END)
+    result = ""
+    if math == 'addition':
+        result = str(f_num + second_number)
+    elif math == "subtraction":
+        result = str(f_num - second_number)
+    elif math == "multiplication":
+        result = str(f_num * second_number)
+    elif math == "division":
+        result = str(f_num // second_number)
+    else :
+        result = "something is wrong!"
+
+    e.insert(0, result)
 
 
-button_equal.grid(row=5, column=1, columnspan=2)
-button_add.grid(row=5, column=0)
+button_1 = Button(root, text='1', padx=40, pady=20, command=lambda : button_click(1))
+button_2 = Button(root, text='2', padx=40, pady=20, command=lambda : button_click(2))
+button_3 = Button(root, text='3', padx=40, pady=20, command=lambda : button_click(3))
+button_4 = Button(root, text='4', padx=40, pady=20, command=lambda : button_click(4))
+button_5 = Button(root, text='5', padx=40, pady=20, command=lambda : button_click(5))
+button_6 = Button(root, text='6', padx=40, pady=20, command=lambda : button_click(6))
+button_7 = Button(root, text='7', padx=40, pady=20, command=lambda : button_click(7))
+button_8 = Button(root, text='8', padx=40, pady=20, command=lambda : button_click(8))
+button_9 = Button(root, text='9', padx=40, pady=20, command=lambda : button_click(9))
+button_0 = Button(root, text='0', padx=40, pady=20, command=lambda : button_click(0))
+button_add = Button(root, text='+', padx=39, pady=20, command= button_add)
+button_equal = Button(root, text='=', padx=91, pady=20, command=button_eql)
+button_clear = Button(root, text='clear', padx=79, pady=20, command= button_clear)
+button_subtract = Button(root, text='-', padx=41, pady=20, command=button_subtract)
+button_multiply = Button(root, text='*', padx=40, pady=20, command=button_multiply)
+button_divide = Button(root, text='/', padx=40, pady=20, command=button_divide)
 
-button_clear.grid(row=4, column=1, columnspan=2)
-button_0.grid(row=4,column=0)
+
+
+button_equal.grid(row=6, column=1, columnspan=2)
+button_add.grid(row=6, column=0)
+
+button_subtract.grid(row=4, column=0)
+button_multiply.grid(row=4, column=1)
+button_divide.grid(row=4, column=2)
+
+button_clear.grid(row=5, column=1, columnspan=2)
+button_0.grid(row=5,column=0)
 
 button_1.grid(row=3,column=0)
 button_2.grid(row=3,column=1)
